755dd6efa394827d0d5cd656ddfc2837f6b7d005
nishantprajapati123 <knishant147@gmail.com>
2020-04-07 03:09:58 +0530

    template updated

--- template.cpp
+++ template.cpp
@@ -1,22 +1,37 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 #define ll long long int
 #define pb push_back
 #define mk make_pair
+#define deb(x) cout << #x << "=" << x << endl
 #define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define PI 3.1415926535897932384626
 #define MOD 1000000007
-#define vi vector < int >
-#define pii pair < int, int >
 #define INF (int)1e9
 #define fastIO ios::sync_with_stdio(0); cin.tie(0);
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+void solve()
+{
+    
+}
 int main()
 {
     fastIO
-    int t;
-    cin>>t;
-    while(t--)
+    int T;
+    scanf("%d", &T);
+    for(int testcase = 1; testcase<=T; testcase++)
     {
-
+        solve();
+        printf("\n");
     }
     return 0;
 }
